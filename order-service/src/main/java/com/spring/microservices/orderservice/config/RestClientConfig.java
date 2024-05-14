d9fe91c8c7a48a02cd773cd2bb49f93da066452d
Susil209 <1807075@kiit.ac.in>
2024-05-15 01:08:54 +0530

    [Migration] from Open feign client to RestClient.

---
+++ order-service/src/main/java/com/spring/microservices/orderservice/config/RestClientConfig.java
@@ -0,0 +1,30 @@
+package com.spring.microservices.orderservice.config;
+
+import com.spring.microservices.orderservice.client.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+// https://docs.spring.io/spring-framework/reference/integration/rest-clients.html
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+
+        return factory.createClient(InventoryClient.class);
+    }
+
+}
